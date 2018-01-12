d77aa1210a8e639616cd0512670ff9d35278b497
Gabriel Sánchez <gabriel@plazapoints.com>
2018-01-12 18:45:21 -0500

    avance landing

--- script/main.js
+++ script/main.js
@@ -27,21 +27,76 @@ $(document).ready(function(){
 		$('body, html').animate({scrollTop: 0}, '2000');
 	});
 
-	//Drag image
-	$('#banner .banner-1[data-orientation!="vertical"]').twentytwenty({
-		default_offset_pct:0.5,
-		no_overlay: true
+	//Slider
+	var banners = $('#banner');
+	var handlers = $('#banner .handlers');
+	var position = 1;
+	var intervalo;
+
+	$.each(banners.find('.banner'), function(index, value){
+		handlers.append('<div class="dot" id="dot' + (index+1) + '" />');
 	});
 
-	//dots
-	var banners = $('#banner .banner');
-	$.each(banners, function(){
-		$('#banner .handlers').append('<div class="dot" />');
-	});
-	$('#banner .handlers .dot').first().addClass('active');
+	banners.find('.banner').first().addClass('active').fadeIn();
+	handlers.find('.dot').first().addClass('active');
 
-	$('body').mousedown(function(ev){
-		
-	})
+	var slide = function(){
+		if(position <= banners.find('.banner').length){
+			banners.find('.banner.active').removeClass('active').next('.banner').addClass('active');
+			banners.find('.banner.active').fadeIn().siblings('.banner').fadeOut();
+			handlers.find('.dot.active').removeClass('active').next().addClass('active');
+			position++;
+			if(position > banners.find('.banner').length){
+				banners.find('.banner').fadeOut();
+				banners.find('.banner').first().fadeIn().addClass('active');
+				handlers.find('.dot').first().addClass('active').siblings().removeClass('active');
+				position = 1;
+			}
+		}
+	}
+
+	intervalo = setInterval(slide, 3000);
+
+	handlers.find('.dot').click(function(ev){
+		var target = ev.target;
+		var id = $(target).attr('id').split('dot')[1];
+		$('#dot' + id).addClass('active').siblings().removeClass('active');
+		position = id;
+		banners.find('.banner').fadeOut();
+		banners.find('.banner-' + id).fadeIn();
+		banners.find('.banner-' + id).addClass('active').siblings('.banner').removeClass('active');
+		clearInterval(intervalo);
+		intervalo = setInterval(slide, 3000);
+	});
+
+	//Lista de beneficios
+	var data = [
+		{
+			image: 'vende-mas-unico-pago.png',
+			title: 'Pago Único',
+			description: 'Esta moderna herramienta puede ser tuya a un costo mínimo. ¡Olvídate de los alquileres mensuales, realiza el primer pago y el MPOS ya es tuyo!'
+		},
+		{
+			image: 'vende-mas-dinero-seguro.png',
+			title: 'Tu dinero está más seguro',
+			description: 'Porque nos preocúpanos por tu seguridad, a través de esta herramienta evitarás acumular mucho efectivo en tu local. De esta manera podrás reducir riesgos de robo.'
+		},
+		{
+			image: 'vende-mas-requisitos.png',
+			title: 'Requisitos Mínimos',
+			description: 'Sólo necesitas de un smartphone con un plan de datos, tu correo electrónico y una cuenta bancaria. ¡Así de simple!'
+		},
+		{
+			image: 'vende-mas-unico-pago.png',
+			title: 'Depósito dentro de las 48 horas útiles',
+			description: 'Ingresa tu cuenta de ahorros o cuenta corriente en concordancia con los datos de tu registro.'
+		}
+	];
+
+	var templateListBeneficios = Handlebars.compile($('#template-list-beneficios').html());
+
+	$.each(data, function(index, value){
+		$('#list-beneficios ul').append(templateListBeneficios(value));
+	});
 
 });
